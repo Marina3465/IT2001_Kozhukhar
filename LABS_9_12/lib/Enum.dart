41741b6f1957951e193c555e4e9e96b9f827bdb8
Marina Kozhukhar <kozhyxarm12@gmail.com>
2023-05-05 22:06:37 +0300

    delete 9-12

--- LABS_9_12/lib/Enum.dart
+++
@@ -1,6 +0,0 @@
-import 'package:labs_9_12/Coffee.dart';
-//перечисление с видами кофе
-
-Coffee espresso = Coffee(50,0,100,70);
-Coffee cappuccino = Coffee(60,60,120,150);
-Coffee latte = Coffee(36,108,72,160);
