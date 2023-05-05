90a40938845d1f7122c1a4dfacd320b92912e311
Marina Kozhukhar <kozhyxarm12@gmail.com>
2023-05-05 22:04:54 +0300

    lab_10 done

--- LABS_9_12/lib/Machine.dart
+++ LABS_9_12/lib/Machine.dart
@@ -1,116 +1,56 @@
+import 'package:labs_9_12/Coffee.dart';
+import 'Resources.dart';
+
 String mess = 'Выберите напиток';
 
 class Machine { //класс кофемашина
-  int _coffeeBeans = 1500;
-  int _milk = 500;
-  int _water = 500;
-  int _cash = 0;
+  Resources _resources = Resources(); //экземпляр класса
 
   Machine() {} //конструктор класса
 
-  int get coffeeBeans { //геттеры
-    return _coffeeBeans;
+  Resources get resources{
+    return _resources;
   }
 
-  int get milk {
-    return _milk;
-  }
-
-  int get water {
-    return _water;
-  }
-
-  int get cash {
-    return _cash;
-  }
-
-  set coffeeBeans(int amount) { //сеттеры
-    _coffeeBeans = amount;
-  }
-
-  set milk(int amount) {
-    _milk = amount;
-  }
-
-  set water(int amount) {
-    _water = amount;
-  }
-
-  set cash(int amount) {
-    _cash = amount;
+  set resources (Resources resources){
+    _resources = resources;
   }
 
   void fillCoffeeBeans(int amount) { //функции добавления ресурсов
-    _coffeeBeans += amount;
+    resources.coffeeBeans += amount;
   }
 
   void fillMilk(int amount) {
-    _milk += amount;
+    resources.milk += amount;
   }
 
   void fillWater(int amount) {
-    _water += amount;
+    resources.water += amount;
   }
 
   void fillCash(int amount){
-    _cash += amount;
+    resources.cash += amount;
   }
 
-  void subtractResourcesForEspresso() { //приготовление кофе и отнимание ресурсов
-    _coffeeBeans -= 50;
-    _water -= 100;
+  void subtractResourcesForCoffee(Coffee coffee){
+    resources.coffeeBeans -= coffee.coffeeBeans;
+    resources.milk -= coffee.milk;
+    resources.water -= coffee.water;
+    resources.cash += coffee.cash;
   }
 
-  void subtractResourcesForCappuccino() {
-    _coffeeBeans -= 60;
-    _water -= 120;
-    _milk -= 60;
+  bool isAvailableForCoffee(Coffee coffee){
+    return(resources.coffeeBeans >= coffee.coffeeBeans &&
+    resources.milk >= coffee.milk &&
+    resources.water >= coffee.water);
   }
 
-  void subtractResourcesForLatte() {
-    _coffeeBeans -= 36;
-    _water -= 72;
-    _milk -= 108;
-  }
-
-  bool isAvailableForEspresso() { //проверка на наличие ресурсов
-    return _coffeeBeans >= 50 && _water >= 100;
-  }
-
-  bool isAvailableForCappuccino() {
-    return _coffeeBeans >= 60 && _water >= 120 && _milk >= 60;
-  }
-
-  bool isAvailableForLatte() {
-    return _coffeeBeans >= 36 && _water >= 72 && _milk >= 108;
-  }
-
-  void makingCoffee(String coffeeType) { //функция приготовления напитка
-    switch (coffeeType) { //переключатель
-      case 'espresso':
-        if (isAvailableForEspresso()) {
-          subtractResourcesForEspresso();
-          mess = 'Ваш эспрессо готов!';
+  void makingCoffee(Coffee coffee) { //функция приготовления напитка
+        if (isAvailableForCoffee(coffee)) {
+          subtractResourcesForCoffee(coffee);
+          mess = 'Ваш кофе готов!';
         } else {
           mess = 'Недостаточно ресурсов, чтобы приготовить кофе :(';
         }
-        break;
-      case 'cappuccino':
-        if (isAvailableForCappuccino()) {
-          subtractResourcesForCappuccino();
-          mess = 'Ваш капучино готов!';
-        } else {
-          mess = 'Недостаточно ресурсов, чтобы приготовить кофе :(';
-        }
-        break;
-      case 'latte':
-        if (isAvailableForLatte()) {
-          subtractResourcesForLatte();
-          mess = 'Ваш латте готов!';
-        } else {
-          mess = 'Недостаточно ресурсов, чтобы приготовить кофе :(';
-        }
-        break;
-    }
   }
 }
--- LABS_9_12/lib/main.dart
+++ LABS_9_12/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Machine.dart'; //ссылка на файл с классом
+import 'package:labs_9_12/Enum.dart'; //ссылка на файл с перечислениями
 
 void main() {
   runApp(MyApp());
@@ -14,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,25 +32,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   final _formKey = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   _MyHomePageState() {
-    coff = coffeeMachine.coffeeBeans; //назначение переменным гет
-    mil = coffeeMachine.milk;
-    wat = coffeeMachine.water;
-    money = coffeeMachine.cash;
+    coff = coffeeMachine.resources.coffeeBeans; //назначение переменным гет
+    mil = coffeeMachine.resources.milk;
+    wat = coffeeMachine.resources.water;
+    money = coffeeMachine.resources.cash;
   }
 
   Machine coffeeMachine = Machine(); //экземпляр класса кофемашины
@@ -80,7 +51,8 @@ class _MyHomePageState extends State<MyHomePage> {
   var money;
 
   @override
-  Widget build(BuildContext context) { //построение экрана
+  Widget build(BuildContext context) {
+    //построение экрана
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -89,7 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text( //вывод на экран того, что осталось из ресурсов
+            Text(
+              //вывод на экран того, что осталось из ресурсов
               'Кофейные зерна: $coff',
               style: TextStyle(fontSize: 15.0),
             ),
@@ -105,62 +78,69 @@ class _MyHomePageState extends State<MyHomePage> {
               'Деньги: $money',
               style: TextStyle(fontSize: 15.0),
             ),
-            ElevatedButton( //кнопка для приготовления эспрессо
+            ElevatedButton(
+                //кнопка для приготовления эспрессо
                 onPressed: () {
-                  coffeeMachine.makingCoffee('espresso');
+                  coffeeMachine
+                      .makingCoffee(espresso); //проверка + изготовление кофе
                   setState(() {
-                    if (coffeeMachine.isAvailableForEspresso())
-                      coffeeMachine.fillCash(70);
-                    coff = coffeeMachine.coffeeBeans;
-                    mil = coffeeMachine.milk;
-                    wat = coffeeMachine.water;
-                    money = coffeeMachine.cash;
+                    coff = coffeeMachine
+                        .resources.coffeeBeans; //убавление ресурсов
+                    mil = coffeeMachine.resources.milk;
+                    wat = coffeeMachine.resources.water;
+                    money = coffeeMachine.resources.cash;
 
                     mess;
                   });
                 },
                 child: Text('Эспрессо 70p')),
-            ElevatedButton( //кнопка для приготовление копучино
+            ElevatedButton(
+                //кнопка для приготовление копучино
                 onPressed: () {
-                  coffeeMachine.makingCoffee('cappuccino');
+                  coffeeMachine
+                      .makingCoffee(cappuccino); //проверка + изготовление кофе
                   setState(() {
-                    if (coffeeMachine.isAvailableForCappuccino())
-                      coffeeMachine.fillCash(150);
-                    coff = coffeeMachine.coffeeBeans;
-                    mil = coffeeMachine.milk;
-                    wat = coffeeMachine.water;
-                    money = coffeeMachine.cash;
+                    coff = coffeeMachine
+                        .resources.coffeeBeans; //убавление ресурсов
+                    mil = coffeeMachine.resources.milk;
+                    wat = coffeeMachine.resources.water;
+                    money = coffeeMachine.resources.cash;
                     mess;
                   });
                 },
                 child: Text('Капучино 150p')),
-            ElevatedButton( //кнопка для приготовления латте
+            ElevatedButton(
+                //кнопка для приготовления латте
                 onPressed: () {
-                  coffeeMachine.makingCoffee('latte');
+                  coffeeMachine
+                      .makingCoffee(latte); //проверка + изготовление кофе
                   setState(() {
-                    if (coffeeMachine.isAvailableForLatte())
-                      coffeeMachine.fillCash(160);
-                    coff = coffeeMachine.coffeeBeans;
-                    mil = coffeeMachine.milk;
-                    wat = coffeeMachine.water;
-                    money = coffeeMachine.cash;
+                    coff = coffeeMachine
+                        .resources.coffeeBeans; //убавление ресурсов
+                    mil = coffeeMachine.resources.milk;
+                    wat = coffeeMachine.resources.water;
+                    money = coffeeMachine.resources.cash;
                     mess;
                   });
                 },
                 child: Text('Латте 160p')),
-            const Text( //простринство для разделения
+            const Text(
+              //простринство для разделения
               '',
             ),
-            Text( //сообщение о приготовлении кофе
+            Text(
+              //сообщение о приготовлении кофе
               '$mess',
               style: Theme.of(context).textTheme.headlineSmall,
             ),
             Form(
-              key: _formKey,
+                key: _formKey,
                 child: Column(
                   children: [
-                    TextFormField( //добавление кофейных зерен
-                        decoration: const InputDecoration(labelText: 'Кофейные зерна '),
+                    TextFormField(
+                        //добавление кофейных зерен
+                        decoration:
+                            const InputDecoration(labelText: 'Кофейные зерна '),
                         keyboardType: TextInputType.number,
                         validator: (value) {
                           //проверка ввода
@@ -184,7 +164,8 @@ class _MyHomePageState extends State<MyHomePage> {
                             return e.toString();
                           }
                         }),
-                    TextFormField( //добавление молока
+                    TextFormField(
+                        //добавление молока
                         decoration: const InputDecoration(labelText: 'Молоко '),
                         keyboardType: TextInputType.number,
                         validator: (value) {
@@ -209,7 +190,8 @@ class _MyHomePageState extends State<MyHomePage> {
                             return e.toString();
                           }
                         }),
-                    TextFormField( //добавление воды
+                    TextFormField(
+                        //добавление воды
                         decoration: const InputDecoration(labelText: 'Вода '),
                         keyboardType: TextInputType.number,
                         validator: (value) {
@@ -234,7 +216,8 @@ class _MyHomePageState extends State<MyHomePage> {
                             return e.toString();
                           }
                         }),
-                    ElevatedButton( //кнопка для добавления ресурсов
+                    ElevatedButton(
+                        //кнопка для добавления ресурсов
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {
                             if (amount_cof > 0 &&
@@ -242,13 +225,15 @@ class _MyHomePageState extends State<MyHomePage> {
                                 amount_water > 0) {
                               //если в переменных есть числа
                               setState(() {
-                                coffeeMachine.fillCoffeeBeans(amount_cof); //вызов сеттеров
+                                coffeeMachine.fillCoffeeBeans(
+                                    amount_cof); //вызов сеттеров
                                 coffeeMachine.fillMilk(amount_milk);
                                 coffeeMachine.fillWater(amount_water);
                               });
-                              coff = coffeeMachine.coffeeBeans; //вывод на экран количества
-                              mil = coffeeMachine.milk;
-                              wat = coffeeMachine.water;
+                              coff = coffeeMachine.resources
+                                  .coffeeBeans; //вывод на экран количества
+                              mil = coffeeMachine.resources.milk;
+                              wat = coffeeMachine.resources.water;
                               amount_cof = 0;
                               amount_milk = 0;
                               amount_water = 0;
@@ -260,8 +245,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           style: TextStyle(color: Colors.white),
                         )),
                   ],
-                )
-            )
+                ))
           ],
         ),
       ),
